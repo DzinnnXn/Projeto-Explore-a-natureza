8c8145c6d74d2aae46cecb1d4b5d8a2337a45ef2
DzinnnXn <andresilvax444@gmail.com>
2023-09-19 14:17:14 -0300

    up

--- js/faq.js
+++ js/faq.js
@@ -1,21 +1,20 @@
 const imagens = document.querySelectorAll('.scroll img');
 const textos = document.querySelectorAll('.textos-main');
-
 imagens.forEach((imagem, index) => {
     imagem.addEventListener('click', () => {
       // Oculta os textos
     textos.forEach(texto => {
         texto.style.display = 'none';
     });
-
-
     textos[index].style.display = 'block';
     });
 });
-    const faqs = document.querySelectorAll(".faq");
 
+// FAQ
+const faqs = document.querySelectorAll(".faq");
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active");
     });
 });
+
