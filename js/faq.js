19962c49e01581f2be5a8019072dfd76bf3cec8e
E_o_dede <140509312+DzinnnXn@users.noreply.github.com>
2023-10-09 19:21:38 -0300

    Update faq.js

--- js/faq.js
+++ js/faq.js
@@ -1,20 +1,7 @@
-const imagens = document.querySelectorAll('.scroll img');
-const textos = document.querySelectorAll('.textos-main');
-imagens.forEach((imagem, index) => {
-    imagem.addEventListener('click', () => {
-      // Oculta os textos
-    textos.forEach(texto => {
-        texto.style.display = 'none';
-    });
-    textos[index].style.display = 'block';
-    });
-});
-
-// FAQ
 const faqs = document.querySelectorAll(".faq");
+
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active");
     });
 });
-
